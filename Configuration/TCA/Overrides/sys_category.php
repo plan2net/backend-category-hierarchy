c1aaa8731a0cd1c7711289b11b6db323d9587ed7
Wolfgang Klinger <wk@plan2.net>
2026-05-17 07:27:55 +0200

    Drop legacy defined('TYPO3') guard from TCA override

--- Configuration/TCA/Overrides/sys_category.php
+++ Configuration/TCA/Overrides/sys_category.php
@@ -1,10 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
 use Plan2net\BackendCategoryHierarchy\CategoryLabelProcessor;
 
-defined('TYPO3') or exit;
-
-(static function () {
-    // This extension "suggests" `georgringer/news` in order to be able to overwrite this user function for categories
-    $GLOBALS['TCA']['sys_category']['ctrl']['label_userFunc'] = CategoryLabelProcessor::class . '->process';
-})();
+// This extension "suggests" `georgringer/news` in order to be able to overwrite this user function for categories.
+$GLOBALS['TCA']['sys_category']['ctrl']['label_userFunc'] = CategoryLabelProcessor::class.'->process';
